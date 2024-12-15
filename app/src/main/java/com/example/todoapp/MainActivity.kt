98eb6a679d1b9284fec96646b4f1bda1cb6d52b5
taramenjakmaksimovic <taramm19@gmail.com>
2024-12-15 18:58:25 +0100

    Todo APP commit

---
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.todoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModelProvider
+import com.example.todoapp.ui.theme.TodoAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val todoViewModel = ViewModelProvider(this)[TodoViewModel::class.java]
+        enableEdgeToEdge()
+        setContent {
+            TodoAppTheme {
+                Surface (modifier = Modifier.fillMaxSize(),
+                    color= MaterialTheme.colorScheme.background
+                ) {
+                    TodoListPage(todoViewModel)
+                }
+            }
+        }
+    }
+}
+
